db5b34b5575f7c820054f05281bb8e2eda7b0466
renesanger <renejsanger@gmail.com>
2025-07-21 14:05:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import FilterableProductTable from "./components/FilterableProductTable";
+import "./App.css";
 
 function App() {
+  const products = [
+    { category: "Fruits", name: "Apple", price: 1, stocked: true },
+    { category: "Fruits", name: "Banana", price: 3, stocked: true },
+    { category: "Fruits", name: "Mango", price: 2, stocked: false },
+    { category: "Vegetable", name: "Kale", price: 1, stocked: true },
+    { category: "Vegetable", name: "Spinach", price: 2, stocked: false },
+    { category: "Vegetable", name: "Brocolli", price: 2, stocked: true },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterableProductTable products={products} />
     </div>
   );
 }
